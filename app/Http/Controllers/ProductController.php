95f0c149aa8bda8193d6489cdb12bbac54e48298
Paweł Dahlke <dahlkepawelcontact@gmail.com>
2023-06-19 22:13:37 +0200

    Crud for products

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use PHPUnit\Logging\Exception;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index():View
+    {
+        return view('products.index',[
+            'products'=>Product::paginate(10)
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create():View
+    {
+        return view('products.create');
+
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $product=new Product($request->all());
+        $product->save();
+        return redirect(route('products.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        return view('products.show',[
+        'product'=>$product
+        ]);
+    }
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product):View
+    {
+        return view('products.edit',[
+            'product'=>$product
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        $product->fill($request->all());
+        $product->save();
+        return redirect(route('products.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        try {
+            $product->delete();
+            return response()->json([
+                'status'=>'succes'
+            ]);
+        }catch (Exception $e){
+            return response()->json([
+                'status'=>'error',
+                'message'=>'Wystąpił błąd!'
+            ])->setStatusCode(500);
+        }
+    }
+
+}
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+        <div class="col-6">
+            <h1>Lista Produktów</h1>
+        </div>
+            <div class="col-6">
+                <a class="float-end" href="{{route('products.create')}}">
+                <button type="button" class="btn btn-primary">Dodaj</button>
+                </a>
+            </div>
+        </div>
+        <div class="row">
+        <table class="table table-hover">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nazwa</th>
+                <th scope="col">Opis</th>
+                <th scope="col">Ilość</th>
+                <th scope="col">Cena</th>
+                <th scope="col">Akcje</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($products as $product)
+                <tr>
+                    <th scope="row">{{$product->id}}</th>
+                    <td>{{$product->name}}</td>
+                    <td>{{$product->description}}</td>
+                    <td>{{$product->amount}}</td>
+                    <td>{{$product->price}}</td>
+                    <td>
+                        <a href="{{route('products.show',$product->id)}}">
+                            <button class="btn btn-success">
+                                Podgląd
+                            </button>
+                        </a>
+                        <a href="{{route('products.edit',$product->id)}}">
+                            <button class="btn btn-info">
+                                Edytuj
+                            </button>
+                        </a>
+                        <button class="btn delete btn-danger btn-sm" data-id="{{$product->id}}">Usuń</button>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+
+        {{ $products->links() }}
+        <div/>
+        </div>
+        @endsection
+        @section('javascript')
+            const deleteUrl="{{url('products')}}/";
+        @endsection
+        @section('js-files')
+            @vite('resources/js/delete.js');
+@endsection
